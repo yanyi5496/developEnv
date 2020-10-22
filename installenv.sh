01aaaef68b90fdedbf83e156c428630ef13ab580
chenqiang <qiang.chen@han-networks.com>
2020-10-22 17:13:58 +0800

    xx

--- installenv.sh
+++ installenv.sh
@@ -8,18 +8,17 @@ echo -e "开始安装 \n"
 apt-get install -y docker.io
 
 docker -v
-echo -e "check docker-compose is exist \n"
+echo -e "\n 检查docker-compose是否已安装 \n"
 
 if [[ ! -x "$dockerPath" ]];then
         echo -e "下载 docker-compose \n"
         sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 fi
 
-echo -e "docker-compose downloaded \n"
 sudo chmod +x /usr/local/bin/docker-compose
 echo "docker-compose 安装成功 "
 docker-compose --version
 
-echo -e "准备运行 \n"
+echo -e "启动docker容器中 \n"
 docker-compose up -d
 
