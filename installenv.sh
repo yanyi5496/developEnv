d50d8c50511b8f7bdf8cee8eeb940fb22dc963b4
chenqiang <qiang.chen@han-networks.com>
2020-10-23 15:30:47 +0800

    fix bug

--- installenv.sh
+++ installenv.sh
@@ -12,6 +12,14 @@ echo -e "开始安装 \n"
 
 apt-get install -y docker.io
 
+echo -e "更换docker为国内源USTC \n"
+
+sudo echo "DOCKER_OPTS=\"--registry-mirror=https://docker.mirrors.ustc.edu.cn\"" >> /etc/default/docker
+
+echo -e "重启docker服务"
+
+sudo service docker restart
+
 echo -e "\n 检查docker-compose是否已安装 \n"
 
 if [[ ! -x "$dockerPath" ]];then
